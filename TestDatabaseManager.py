de5583f9da3ca04986df984d7214b2fec8270b17
sanelakca <sanel.akca.2020@live.rhul.ac.uk>
2024-02-01 16:56:56 +0000

    Added a test case to creating orders

--- TestDatabaseManager.py
+++ TestDatabaseManager.py
@@ -38,3 +38,18 @@ def test_create_duplicate_user(db_manager):
     with pytest.raises(sqlite3.IntegrityError):
         db_manager.create_user(*user_data)
 
+def test_create_order(db_manager):
+    order_data = ("2024-01-01 00:00:00", "John Porks Order", 1, 50.00, 1)
+    db_manager.create_order(*order_data)
+
+    db_manager.db_cursor.execute("SELECT * FROM orders WHERE customer_name = 'John Porks Order")
+    order = db_manager.db_cursor.fetchone()
+
+    assert order is not None
+    assert order[1] == "2024-01-01 00:00:00"
+    assert order[2] == "John Porks Order"
+    assert order[3] == 1 #table number
+    assert order[4] == 50.00
+    assert order[5] == 1 #user id
+
+    db_manager.db_connection.close()
