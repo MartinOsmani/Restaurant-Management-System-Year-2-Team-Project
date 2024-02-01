708e3c79fad55ad237e9d23518ff7159bdb91046
zlac158 <zlac158@live.rhul.ac.uk>
2024-02-01 13:43:53 +0000

    Add test for database creation

---
+++ TestDatabaseManager.py
@@ -0,0 +1,26 @@
+from InitDatabase import init_db
+import pytest
+import sqlite3
+import os
+
+
+def check_table_exists(db_connection, table_name):
+    cursor = db_connection.cursor()
+    cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}';")
+    return cursor.fetchone() is not None
+
+@pytest.fixture
+def db_connection():
+    os.system("rm test.db")
+    init_db("test.db")
+    connection = sqlite3.connect("test.db")
+  
+    yield connection
+
+    connection.close()
+
+def test_tables_exist(db_connection):
+    assert check_table_exists(db_connection, "users"), "The Table users does not exist"
+    assert check_table_exists(db_connection, "orders"), "The Table orders does not exist"
+    assert check_table_exists(db_connection, "order_items"), "The Table order_items does not exist"
+
