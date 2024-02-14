da5bb2876d649dd689491c881b15ce47d296ba95
sanelakca <sanel.akca.2020@live.rhul.ac.uk>
2024-02-14 15:05:33 +0000

    Updated Menu Items schema to include categories for the menu

--- flaskr/Menu Items.sql
+++ flaskr/Menu Items.sql
@@ -7,5 +7,6 @@ CREATE TABLE menu_items (
   menu_item_price NUMERIC(8,2) NOT NULL,
   menu_item_ingredients TEXT NOT NULL,
   menu_item_calorie INT,
-  menu_item_image_url VARCHAR(255)
+  menu_item_image_url VARCHAR(255),
+  menu_item_categroy VARCHAR(255)
 );
