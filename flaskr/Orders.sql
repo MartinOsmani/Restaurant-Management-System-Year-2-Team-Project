f7282486f611c2205092712c7ea7d6aa12f5bbf7
Martin Osmani <wlis209@live.rhul.ac.uk>
2024-03-07 14:49:09 +0000

    Modified Orders.sql to add order_status that accepts 4 possible statuses.

--- flaskr/Orders.sql
+++ flaskr/Orders.sql
@@ -6,6 +6,11 @@ CREATE TABLE orders (
   email TEXT REFERENCES users(email),
   table_number INTEGER NOT NULL,
   total DECIMAL(8,2) NOT NULL,
-  user_id INTEGER REFERENCES users(user_id)
+  order_status TEXT NOT NULL,
+  user_id INTEGER REFERENCES users(user_id),
+  CHECK (order_status IN ('Order confirmed!',
+                          'The order is in the kitchen!',
+                          'The order is ready and will be with you shortly!',
+                          'The order has been delivered!'))
 );
 
