a6822616139b08dbc5a865cf40ccf17935940c65
Martin Osmani <wlis209@live.rhul.ac.uk>
2024-03-25 02:53:44 +0000

    Added docstrings to db.py file.

--- src/db.py
+++ src/db.py
@@ -5,6 +5,13 @@ import click
 
 
 def get_db():
+    """
+    Creates a database connection using the database path from Flask's 'current_app' config.
+    Accesses SQlite query results by column name.
+
+    Returns:
+        A sqlite3 database connection object.
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -16,6 +23,13 @@ def get_db():
 
 
 def init_db(db_name='database.db'):
+    """
+    Initializes the database by creating tables according to the schema files from
+    'database' directory relative to its location.
+
+    Parameters:
+        db_name (str): The name of the database file. Default is 'database.db'
+    """
     db = get_db()
     sql_dir = os.path.join(os.path.dirname(__file__), 'database')
     sql_files = ['users.sql', 'Orders.sql', 'Order Items.sql', 'Menu Items.sql']
@@ -28,6 +42,12 @@ def init_db(db_name='database.db'):
 
 
 def close_db(e=None):
+    """
+    Closes database connection.
+
+    Parameters:
+        e (Exception, optional): An error instance, default to None.
+    """
     db = g.pop('db', None)
 
     if db is not None:
@@ -35,11 +55,19 @@ def close_db(e=None):
 
 @click.command('init-db')
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    """
+    Command to clear existing data and create new tables in the database.
+    """
     init_db()
     click.echo('Initialized the database.')
 
 def init_app(app):
+    """
+    Registers database-related functions with the Flask application
+
+    Parameters:
+        app (Flask): The Flask application instance.
+    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
